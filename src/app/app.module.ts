fdfecd46167fab5e791a20f9370e843b1579f7c7
cristina <cris78seijas@gmail.com>
2022-05-21 01:04:03 -0300

    componente exp_laboral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExpLaboralComponent } from './componentes/exp-laboral/exp-laboral.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AcercaDeComponent,
-    EducacionComponent
+    EducacionComponent,
+    ExpLaboralComponent
   ],
   imports: [
     BrowserModule,
